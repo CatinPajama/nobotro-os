32ccefb2b9a60c23e0e90cc4c64bda656b892789
Gaurab <gaurabkhatry@gmail.com>
2025-07-19 13:01:05 +0545

    reverting bootloader

--- boot/bootloader.asm
+++ boot/bootloader.asm
@@ -1,16 +1,17 @@
 [org 0x7c00]
-bits 16
  
-jmp short start
-nop
+jmp load
 
-; jump over OEM block
+ 				; jump over OEM block
 
-;-------------------------------------------------
+;*************************************************;
 ;	OEM Parameter block
-;-------------------------------------------------
+;*************************************************;
 
-bpbOEM                  DB "MSWIN4.1"
+start:
+
+times 0Bh-$+start DB 0
+
 bpbBytesPerSector:  	DW 512
 bpbSectorsPerCluster: 	DB 1
 bpbReservedSectors: 	DW 1
@@ -27,161 +28,98 @@ bsDriveNumber: 	        DB 0
 bsUnused: 	            DB 0
 bsExtBootSignature: 	DB 0x29
 bsSerialNumber:	        DD 0xa0a1a2a3
-bsVolumeLabel: 	        DB "nobotro os "
-bsFileSystem: 	        DB "FAT12   "
-
-start:
-    jmp main
-
-puts:
-    push si
-    push ax
-    push bx
-
-.loop:
-    lodsb               ; loads next character in al
-    or al, al           ; verify if next character is null?
-    jz .done
-
-    mov ah, 0x0E        ; call bios interrupt
-    mov bh, 0           ; set page number to 0
-    int 0x10
-
-    jmp .loop
-
-.done:
-    pop bx
-    pop ax
-    pop si    
-    ret
-
-; ------------------------
-; Disk routines
-; ------------------------
-; Parameters:
-;    - ax: LBA Address
-;    - cx: number of sectors
-;    - dl: drive number
-;    - es:bx: store address
-; ------------------------
-
-disk_read:
-    push ax
-    mov [dap_lba], ax      ; lower 16 bits of LBA
-    xor ax, ax
-    mov [dap_lba + 2], ax  ; upper 16 bits of lower dword (set to 0)
-
-    mov word [dap + 4], bx
-    mov word [dap + 6], es
-
-    mov byte [dap], 0x10       ; size of DAP
-    mov byte [dap + 1], 0x00   ; reserved
-    mov word [dap + 2], cx      ; number of sectors
-    ;call lba_to_chs                     ; compute CHS
-    pop ax                              ; AL = number of sectors to read
-    
-    mov ah, 42h
-    mov si, dap
-    int 13h                             ; carry flag cleared = success
-
-    ; Checking carry? Apparently it is always returing error event thought it is
-    ; working. Docs also recommends that we should retry at least 3 times before
-    ; failing when there is disk error
-
-    ret
-
-check_LBA:
-    push ax
-    push bx
-    push cx
-    mov ah, 0x41
-    stc
-    int 13h
-    jnc .present
-    
-    mov si, msg_lba_not_present
-    call puts
-    cli
-    hlt
-
-.present:
-    pop cx
-    pop bx
-    pop ax
-    ret
-
-disk_reset:
-    pusha
-
-    mov ah, 0
-    stc
-    int 13h
-    ;jc floppy_error
-    popa
-    ret
+bsVolumeLabel: 	        DB "nobotro os"
+bsFileSystem: 	        DB "FAT12"
 
 
-load_kernel_data:
-    pusha
-    mov ax, 0x0
-    mov es, ax      ; ES = 0
-    mov bx, 0x1000  ; BX = 0x1000. ES:BX=0x0:0x1000 
-    ; ES:BX = starting address to read sector(s) into
-    mov ax, 1
-    mov cl, 15
-    call disk_read
-    popa
+load:
 
-    ret
-
-main:
-    MOV EBP, 0x7c00         ; Set stack base at top of free space
-    MOV ESP, EBP
-
-    ; DL hasn't been destroyed by our bootloader code and still
-    ;     contains boot drive # passed to our bootloader by the BIOS
-    mov [bsDriveNumber], dl ; dl has been set by bios
-
-    call check_LBA 
-    call disk_reset
-
-    call load_kernel_data
-    jmp load_gdt_and_kernel
-
-;-------------------------------
+MOV EBP, 0x9000                       ; Set stack base at top of free space
+MOV ESP, EBP
+ mov ah,00
+int 13h
 
 
-load_gdt_and_kernel:
-    cli
-    xor ax,ax
-    mov ds, ax 
-      
-    lgdt [gdt_descriptor]
-
-    mov eax , cr0
-    or eax , 0x1
-
-    mov cr0 , eax
-
-    jmp gdt_codeSeg:init_pm
 
 
-    [bits 32]
-    init_pm:
-     
-    MOV AX, gdt_dataSeg                     ; Update segments for protected mode as defined in GDT
-    MOV DS, AX
-    MOV SS, AX
-    MOV ES, AX
 
-    MOV EBP, 0x7c00                       ; Set stack base at top of free space
-    MOV ESP, EBP
+;load kernel to memory
+read_sector:
+pusha
+mov ax, 0x0
+mov es, ax      ; ES = 0
+mov bx, 0x1000  ; BX = 0x1000. ES:BX=0x0:0x1000 
+                ; ES:BX = starting address to read sector(s) into
+mov ah, 02      ; Int 13h/AH=2 = Read Sectors From Drive
+mov al, 15   ; Sectors to read = 15
+mov ch, 00      ; CH=Cylinder. Second sector of disk
+                ; is at Cylinder 0 not 1
+mov cl, 02      ; Sector to read = 2
+mov dh, 00      ; Head to read = 0
+                ; DL hasn't been destroyed by our bootloader code and still
+                ;     contains boot drive # passed to our bootloader by the BIOS
+int 13h
+popa
 
-    call 0x1000;jump to kernel head
-     
-    jmp $
+cli
 
-; Global descriptor table for 32-bit protected mode
+
+ 
+
+xor ax,ax
+mov ds, ax 
+  
+lgdt [gdt_descriptor]
+
+ 
+  
+ mov eax , cr0
+or eax , 0x1
+
+mov cr0 , eax
+
+
+
+jmp gdt_codeSeg:init_pm
+
+
+
+[bits 32]
+
+ 
+init_pm:
+ 
+MOV AX, gdt_dataSeg                     ; Update segments for protected mode as defined in GDT
+MOV DS, AX
+MOV SS, AX
+MOV ES, AX
+; MOV FS, AX
+; MOV GS, AX
+
+MOV EBP, 0x9000                       ; Set stack base at top of free space
+MOV ESP, EBP 
+
+
+
+call 0x1000;jump to kernel head
+ 
+jmp $
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+ ; Global descriptor table for 32-bit protected mode
 gdt_start:                              ; Start of global descriptor table
     gdt_null:                           ; Null descriptor chunk
         dd 0x00
@@ -208,22 +146,7 @@ gdt_descriptor:                         ; Table descriptor
 gdt_codeSeg equ gdt_code - gdt_start    ; Offset of code segment from start
 gdt_dataSeg equ gdt_data - gdt_start    ; Offset of data segment from start
 
-msg_greet:              db 'HELLO FROM THE BOOTLOADER', 0x0D, 0x0A, 0
-msg_lba_present:        db 'LBA PRESENT', 0x0D, 0x0A, 0
-msg_lba_not_present:    db 'LBA NOT PRESENT', 0x0D, 0x0A, 0
-
-; ------------------------
-; Disk Address Packet (DAP)
-; ------------------------
-dap:
-    db 0x10       ; Size
-    db 0x00       ; Reserved
-    dw 1          ; Sectors to read
-    dw 0x0        ; Offset
-    dw 0x0        ; Segment
-dap_lba:      
-    dd 0          ; LBA low 32 bits (AX)
-    dd 0          ; LBA high 32 bits
+; Tail
 
 times 510 -( $ - $$ ) db 0 
 dw 0xaa55
